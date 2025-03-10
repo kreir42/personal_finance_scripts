5b86e7194c124349205ddff0826dfef2de872663
kreir42 <gortle97@gmail.com>
2025-03-10 13:02:15 +0100

    Added benchmark symbol and some extra output to portfolio_report

--- portfolio_report.py
+++ portfolio_report.py
@@ -1,5 +1,8 @@
 base_currency = 'EUR'
+base_currency_symbol = '€'
 cpi_filename = 'EU_cpi.csv'
+benchmark_symbol = 'IMID'
+IRR_plot_skip_days = 60
 
 from datetime import datetime, timezone, timedelta
 import matplotlib.pyplot as plt
@@ -36,6 +39,10 @@ final_cpi = cpi['CPI'].iloc[-1]
 df['CPI'] = cpi['CPI']
 df['CPI'] = df['CPI'].ffill()
 df['Inflation adjustment'] = final_cpi/df['CPI']
+df['Raw net flow'] = 0.0
+df['Net flow'] = 0.0
+df['Benchmark amount'] = 0.0
+df['Benchmark value'] = 0.0
 
 df[base_currency] = 0.0
 
@@ -58,9 +65,17 @@ for symbol in trades['symbol'].unique():
     tmp = pd.read_csv('price_data/'+symbol+'.csv', index_col=0, names=['date','price'], parse_dates=True)
     df[symbol+'_price'] = tmp['price']
     df[symbol+'_price'] = df[symbol+'_price'].ffill().fillna(0)
-#transfers
+#process transfers and calculate net flow of money (money in minus out)
 for date, row in transfers.iterrows():
-    df.loc[date:, row['currency']] += row['amount']
+    currency = row['currency']
+    if currency==base_currency:
+        money = row['amount']
+    else:
+        money = row['amount'] * df[currency+'_price']
+    df.loc[date:, currency] += row['amount']
+    df.loc[date:, 'Raw net flow'] += money
+    df.loc[date:, 'Net flow'] += money * df.loc[date, 'Inflation adjustment']
+    df.loc[date:, 'Benchmark amount'] += money / df.loc[date, benchmark_symbol+'_price']
 #get number of shares over time
 for date, row in trades.iterrows():
     symbol = row['symbol']
@@ -73,6 +88,7 @@ for date, row in trades.iterrows():
     df.loc[date:, symbol] += row['amount']
     df.loc[date:, row['currency']] += row['procceeds']+row['commission']
 #calculate value
+df['Benchmark value'] = df['Benchmark amount'] * df[benchmark_symbol+'_price'] * df['Inflation adjustment']
 df['value'] = df[base_currency]
 for symbol in currencies:
     if symbol==base_currency:
@@ -84,6 +100,7 @@ for symbol in symbols:
 df['value'] *= df['Inflation adjustment']
 
 #calculate IRR
+df['Benchmark IRR'] = 0.0
 df['IRR'] = 0.0
 transfers_array = []
 for date, row in df.iterrows():
@@ -91,15 +108,20 @@ for date, row in df.iterrows():
         transfers_array.append(transfers.loc[date, 'amount']*df.loc[date, 'Inflation adjustment'])
     except:
         transfers_array.append(0)
+    df.loc[date, 'Benchmark IRR'] = calculate_IRR(df.loc[date, 'Benchmark value'], transfers_array)
     df.loc[date, 'IRR'] = calculate_IRR(df.loc[date, 'value'], transfers_array)
+df['Benchmark IRR'] = (pow(df['Benchmark IRR']+1,365.24)-1)*100 #go from daily to yearly percentage
 df['IRR'] = (pow(df['IRR']+1,365.24)-1)*100 #go from daily to yearly percentage
 
 print(df)
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
-ax1.plot(df.index, df['IRR'], label='IRR', color='blue')
-ax1.plot(df.index, df['IRR'].rolling(window=30).mean(), label='30-day average', linestyle='--', linewidth=0.8, color='blue')
+df_subset = df.iloc[IRR_plot_skip_days:]
+ax1.plot(df_subset.index, df_subset['IRR'], label='IRR', color='blue')
+#ax1.plot(df_subset.index, df_subset['IRR'].rolling(window=30).mean(), label='30-day average', linestyle='--', linewidth=0.8, color='blue')
+ax1.plot(df_subset.index, df_subset['Benchmark IRR'], label='Benchmark ('+benchmark_symbol+') IRR', linewidth=0.8, color='green')
 ax2.plot(df.index, df['value'], label='Value', color='black', linestyle='--', linewidth=0.85)
+ax2.plot(df.index, df['Benchmark value'], label='Benchmark ('+benchmark_symbol+') value', linestyle='--', linewidth=0.85, color='green')
 plt.tight_layout()
 ax1.set_xlabel('Date')
 ax1.set_ylabel('IRR (%)')
@@ -108,6 +130,8 @@ ax1.grid(True)
 fig.legend()
 plt.show()
 
+total_commissions = 0
+total_commissions_raw = 0
 #Report IRR per symbol
 print()
 print('IRR per symbol:')
@@ -120,7 +144,20 @@ for symbol in symbols:
             continue
         money_flow.loc[date, 'value'] -= row['procceeds']+row['commission']*df.loc[date, 'Inflation adjustment']
         if row['currency']!=base_currency:
-            money_flow.loc[date, 'value'] *= df.loc[date, row['currency']+'_price']
+            currency_equivalence = df.loc[date, row['currency']+'_price']
+            money_flow.loc[date, 'value'] *= currency_equivalence
+            total_commissions_raw += row['commission'] * currency_equivalence
+            total_commissions += row['commission']*df.loc[date, 'Inflation adjustment'] * currency_equivalence
+        else:
+            total_commissions_raw += row['commission']
+            total_commissions += row['commission']*df.loc[date, 'Inflation adjustment']
     IRR = calculate_IRR(df[symbol].iat[-1]*df[symbol+'_price'].iat[-1], money_flow['value'])
     IRR = (pow(IRR+1,365.24)-1)*100 #go from daily to yearly percentage
     print(f"    {symbol}: {IRR:.2f}%")
+print()
+print()
+current_value = df['value'].iat[-1]
+print(f"Current value:   {current_value:.2f}"+base_currency_symbol)
+print(f"Net flow:        {df['Raw net flow'].iat[-1]:.2f}"+base_currency_symbol+f"     -->     Inflation adjusted: {df['Net flow'].iat[-1]:.2f}"+base_currency_symbol)
+print(f"Current P&L:     {current_value-df['Raw net flow'].iat[-1]:.2f}"+base_currency_symbol+f"     -->     Inflation adjusted: {current_value-df['Net flow'].iat[-1]:.2f}"+base_currency_symbol)
+print(f"Total fees paid: {-total_commissions_raw:.2f}"+base_currency_symbol+f"     -->     Inflation adjusted: {-total_commissions:.2f}"+base_currency_symbol)
